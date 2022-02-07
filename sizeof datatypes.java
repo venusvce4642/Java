23cb0c607f8227e0580ea744b5e97afb34ec0096
Venugopal Reddy N <88315767+venusvce4642@users.noreply.github.com>
2022-02-07 14:55:58 +0530

    Create sizeof datatypes.java

---
+++ sizeof datatypes.java
@@ -0,0 +1,19 @@
+import java.util.*; // we need to import this package for to use Scanner class
+public class Main
+{
+	public static void main(String[] args) {
+
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);// to find the range of integer using builtint Integer class
+		System.out.println(Integer.BYTES);
+	  System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);// to find the range of integer using builtint Byte class
+		System.out.println(Byte.BYTES);
+	  System.out.println(Float.MAX_VALUE);
+		System.out.println(Float.MIN_VALUE);// to find the range of integer using builtin Float class
+		System.out.println(Float.BYTES);
+	
+
+	}
+}
